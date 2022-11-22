f002238a0d76d790b31258433f442b5aad7497f5
nepalsandesh <sandeshnepal43@gmail.com>
2022-11-22 23:41:20 +0545

    some variables implementation update

--- main.py
+++ main.py
@@ -10,21 +10,25 @@ resolution = (width, height)
 pygame.init()
 screen = pygame.display.set_mode(resolution)
 clock = pygame.time.Clock()
-FPS = 20
+FPS = 30
 
 # colors
 black = (0,0,0)
 white = (255, 255, 255)
 
+scale = 30 # scale-offset would be the actual width or height of each cell
+offset = 2  # for spacing between cells
 
-Grid = grid.Grid(width, height, 5, 1)
+
+Grid = grid.Grid(width, height, scale, offset)
 Grid.random2d_array()
 
 pause = False
 
 while True:
     clock.tick(FPS)
-    pygame.display.set_caption("{}".format(clock.get_fps()))
+    # pygame.display.set_caption("{}".format(clock.get_fps()))
+    pygame.display.set_caption("Conway's Game of Life")
     screen.fill(black)
     
     for event in pygame.event.get():
